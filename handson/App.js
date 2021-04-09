1cd8e08b3a00938770f072f3218802f64e0c469d
uh-zz <uhzz.contact@gmail.com>
2021-04-10 00:14:27 +0900

    add modal action

--- handson/App.js
+++ handson/App.js
@@ -8,16 +8,21 @@ import {
   SafeAreaView,
   TextInput,
   FlatList,
+  Modal,
+  Pressable,
 } from "react-native";
 
 import { Ionicons } from "@expo/vector-icons";
 
 function Input(props) {
   const [text, setText] = useState("");
+
+  // ボタン押したときの挙動
   const onPress = () => {
     text ? props.addEet(text) : null;
     setText("");
   };
+
   return (
     <View style={styles.inputContainer}>
       <TextInput
@@ -26,14 +31,19 @@ function Input(props) {
         value={text}
       />
       <TouchableOpacity style={styles.button} onPress={onPress}>
-        <Text style={styles.buttonText}>イートする</Text>
+        <Text style={styles.buttonText}>ツ○ートする</Text>
       </TouchableOpacity>
     </View>
   );
 }
 
 export default function App() {
+  // ツイートのステート
   const [eet, setEet] = useState([]);
+
+  // ツイートをリストに追加する
+  // 新しいツイートを上位に追加する
+  // (配列の先頭に追加する
   const addEet = (text) => {
     const newEet = [].concat(eet);
     const nowDate = new Date();
@@ -46,12 +56,15 @@ export default function App() {
     console.log(newEet);
     setEet(newEet);
   };
+
+  // いいね機能
   const onLike = (index) => {
     const newEet = [].concat(eet);
     newEet[index].like = !newEet[index].like;
     setEet(newEet);
   };
 
+  // 削除機能
   const onDelete = (index) => {
     const newEet = [].concat(eet);
     newEet.splice(index, 1);
@@ -129,30 +142,112 @@ const styles = StyleSheet.create({
   contentContainer: {
     paddingBottom: 50,
   },
+  centeredView: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 22,
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: "white",
+    borderRadius: 20,
+    padding: 35,
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  buttonOpen: {
+    backgroundColor: "#F194FF",
+  },
+  buttonClose: {
+    backgroundColor: "#2196F3",
+  },
+  textStyle: {
+    color: "white",
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+  modalText: {
+    marginBottom: 15,
+    textAlign: "center",
+  },
 });
 
 function Eet(props) {
   const { text, like, onLike, date, onDelete } = props;
+
+  // モーダル用
+  const [modalVisible, setModalVisible] = useState(false);
+
+  // モーダル表示
+  const showModal = () => {
+    setModalVisible(true);
+  };
+
+  // モーダル非表示
+  const closeModal = () => {
+    setModalVisible(false);
+  };
+
+  // モーダルのテキスト状態保持
+  const [modalText, setModalText] = useState("");
   return (
-    <View style={eetStyles.container}>
-      <Text style={eetStyles.text}>{text}</Text>
-      <TouchableOpacity onPress={onDelete}>
-        <Ionicons name="close" size={22} color="rgb(252, 108, 133)" />
+    <View>
+      <TouchableOpacity style={eetStyles.container} onPress={() => showModal()}>
+        <View style={eetStyles.subContainer}>
+          <Text style={eetStyles.text}>{text}</Text>
+          <TouchableOpacity onPress={onDelete}>
+            <Ionicons name="close" size={22} color="rgb(252, 108, 133)" />
+          </TouchableOpacity>
+        </View>
+        <View style={eetStyles.actionContainer}>
+          <TouchableOpacity onPress={onLike}>
+            {like ? (
+              <Ionicons
+                name="heart-circle-sharp"
+                size={22}
+                color="rgb(252, 108, 133)"
+              />
+            ) : (
+              <Ionicons
+                name="ios-heart-circle-outline"
+                size={22}
+                color="#aaa"
+              />
+            )}
+          </TouchableOpacity>
+          <Text style={eetStyles.date}>{date}</Text>
+        </View>
       </TouchableOpacity>
-      <View style={eetStyles.actionContainer}>
-        <TouchableOpacity onPress={onLike}>
-          {like ? (
-            <Ionicons
-              name="heart-circle-sharp"
-              size={22}
-              color="rgb(252, 108, 133)"
-            />
-          ) : (
-            <Ionicons name="ios-heart-circle-outline" size={22} color="#aaa" />
-          )}
-        </TouchableOpacity>
-        <Text style={eetStyles.date}>{date}</Text>
-      </View>
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => {
+          Alert.alert("Modal has been closed.");
+          setModalVisible(!modalVisible);
+        }}
+      >
+        <View style={styles.centeredView}>
+          <View style={styles.modalView}>
+            <TextInput style={styles.modalText}>Hello World!</TextInput>
+
+            <Pressable
+              style={[styles.button, styles.buttonClose]}
+              onPress={() => closeModal()}
+            >
+              <Text style={styles.textStyle}>Hide Modal</Text>
+            </Pressable>
+          </View>
+        </View>
+      </Modal>
     </View>
   );
 }
@@ -167,6 +262,10 @@ const eetStyles = StyleSheet.create({
     borderRadius: 5,
     // flexDirection: "row",
     // justifyContent: "space-between",
+  },
+  subContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
   },
   text: {
     color: "white",
